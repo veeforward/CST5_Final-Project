bafca7ab4f0b6c68d9342ce765ab39ad6ae6ca62
BabyBokChoy8 <160865351+BabyBokChoy8@users.noreply.github.com>
2024-03-09 02:08:37 +0800

    Add files via upload

--- BabyBokChoy's updated codes/checkout.js
+++ BabyBokChoy's updated codes/checkout.js
@@ -10,7 +10,7 @@ function checkCart(){
 checkCart();
 addCartToHTML();
 function addCartToHTML(){
-    // clear data default
+
     let listCartHTML = document.querySelector('.returnCart .list');
     listCartHTML.innerHTML = '';
 
@@ -18,7 +18,7 @@ function addCartToHTML(){
     let totalPriceHTML = document.querySelector('.totalPrice');
     let totalQuantity = 0;
     let totalPrice = 0;
-    // if has product in Cart
+ 
     if(listCart){
         listCart.forEach(product => {
             if(product){
@@ -28,10 +28,10 @@ function addCartToHTML(){
                     `<img src="${product.image}">
                     <div class="info">
                         <div class="name">${product.name}</div>
-                        <div class="price">$${product.price}/1 product</div>
+                        <div class="price">₱${product.price}/1 product</div>
                     </div>
                     <div class="quantity">${product.quantity}</div>
-                    <div class="returnPrice">$${product.price * product.quantity}</div>`;
+                    <div class="returnPrice">₱${product.price * product.quantity}</div>`;
                 listCartHTML.appendChild(newCart);
                 totalQuantity = totalQuantity + product.quantity;
                 totalPrice = totalPrice + (product.price * product.quantity);
@@ -39,5 +39,5 @@ function addCartToHTML(){
         })
     }
     totalQuantityHTML.innerText = totalQuantity;
-    totalPriceHTML.innerText = '$' + totalPrice;
+    totalPriceHTML.innerText = '₱' + totalPrice;
 }
